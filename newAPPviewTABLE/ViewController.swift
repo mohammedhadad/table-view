4d24e4921e638b7e96c19ba8c4c6a4d0ba72f5e1
mohamed <mohamed@mohameds-mac.local>
2016-07-30 18:36:34 +0100

    Initial Commit

---
+++ newAPPviewTABLE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newAPPviewTABLE
+//
+//  Created by mohamed on 7/30/16.
+//  Copyright © 2016 mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
